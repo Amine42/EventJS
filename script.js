14091921b6cc91c987080f03798f62985ea85789
Malik42 <malik.ahres@hotmail.fr>
2019-02-25 02:25:36 +0100

    functionality 1 finish

---
+++ script.js
@@ -0,0 +1,16 @@
+// Projet : Faire une page interactive avec JS
+
+// Fonctionnalité 1 :
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+let footer = document.querySelector('footer')
+
+footer.addEventListener('click',posterOnTheConsole);
+
+function posterOnTheConsole()
+{
+    console.log("clique");
+}
+
+/* -------------------------------------------------------------------------------------------------------- */
