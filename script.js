3d33fbbec8d1ebe80dc949ecca64cfaf078b96ec
Amine42 <amine.bouraghda@gmail.com>
2019-02-25 23:36:31 +0100

    ex 6 finish

--- script.js
+++ script.js
@@ -108,3 +108,30 @@ function atomic()
 navbar.addEventListener("dblclick",atomic);
 
 /* -------------------------------------------------------------------------------------------------------- */
+
+// Fonctionnalité 5 :
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+let cards = document.getElementsByClassName("card");
+let button_view = document.getElementsByClassName("btn-success");
+
+
+for(let i = 0; i < cards.length; i++)
+{
+    function view()
+    {
+        if (cards[i].getElementsByClassName("card-text")[0].style.display != "none")
+        {
+            cards[i].getElementsByClassName("card-text")[0].style.display = "none";
+            cards[i].getElementsByClassName("card-img-top")[0].style.width = "20%";
+        }
+        else
+        {
+            cards[i].getElementsByClassName("card-text")[0].style.display = "block";
+            cards[i].getElementsByClassName("card-img-top")[0].style.width = "100%";
+        }
+    }
+
+    button_view[i].addEventListener("mouseover", view);
+}
